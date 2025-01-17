067992098e94d3bc16f16adeb69dfaa53e73cd62
Matt Roumeliotis <mattromo27@gmail.com>
2025-01-16 21:06:59 -0500

    Update ball->paddle collision to change the direction based on where the ball hit the paddle

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 class_name Ball
 
 const BALL_SIZE := 30
+const MAX_BOUNCE_ANGLE := PI / 4
 
 const INITIAL_SPEED := 250.0
 const SPEED_MULTIPLIER := 1.05
@@ -52,16 +53,31 @@ func handle_collision_with_object(object: Object):
 		switch_vertical_direction()
 		increase_speed()
 		hit_wall_sound.play()
-	else:
-		switch_horizontal_direction()
+	elif object is Paddle:
+		bounce_off_of_paddle(object as Paddle)
 		increase_speed()
 		hit_paddle_sound.play()
 
 func switch_vertical_direction() -> void:
 	direction_vector.y = -direction_vector.y
 	
-func switch_horizontal_direction() -> void:
-	direction_vector.x = -direction_vector.x 
+func bounce_off_of_paddle(paddle: Paddle) -> void:	
+	var relative_intersect_y := paddle.get_center_y_coordinate() - get_center_y_coordinate()
+	
+	var half_paddle_height := paddle.HEIGHT / 2
+	var normalized_intersection := (relative_intersect_y / half_paddle_height) * ((PI / 2) - MAX_BOUNCE_ANGLE)
+	
+	var bounce_angle = normalized_intersection * MAX_BOUNCE_ANGLE
+	
+	if direction_vector.x > 0:
+		direction_vector.x = direction_vector.length() * cos(bounce_angle) * -1
+		direction_vector.y = direction_vector.length() * sin(bounce_angle) * -1
+	else:
+		direction_vector.x = direction_vector.length() * cos(bounce_angle)
+		direction_vector.y = direction_vector.length() * -sin(bounce_angle)
+	
+func get_center_y_coordinate() -> float:
+	return position.y + BALL_SIZE / 2
 
 func increase_speed() -> void:
 	speed *= SPEED_MULTIPLIER
--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -5,3 +5,6 @@ class_name Paddle
 const WIDTH := 25
 const HEIGHT := 100
 const SPEED := 250
+
+func get_center_y_coordinate() -> float:
+	return position.y + HEIGHT / 2

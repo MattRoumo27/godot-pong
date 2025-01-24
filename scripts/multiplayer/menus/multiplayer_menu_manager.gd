8ddb58bff192051206885911809c68395ad14d1d
Matt Roumeliotis <mattromo27@gmail.com>
2025-01-23 22:10:06 -0500

    Add a vertical container to group the host and join UI buttons; Use a MenuManager node/script to manage the multiplayer menu logic

---
+++ scripts/multiplayer/menus/multiplayer_menu_manager.gd
@@ -0,0 +1,18 @@
+extends Node
+
+@onready var multiplayer_container: VBoxContainer = $MultiplayerContainer
+@onready var host_button: Button = $MultiplayerContainer/HostButton
+@onready var join_button: Button = $MultiplayerContainer/JoinButton
+
+func _ready() -> void:
+	host_button.button_up.connect(_on_host_button_button_up)
+	join_button.button_up.connect(_on_join_button_button_up)
+	
+
+func _on_host_button_button_up() -> void:
+	Lobby.create_game()
+	multiplayer_container.hide()
+
+
+func _on_join_button_button_up() -> void:
+	pass # Replace with function body.
